cd9d917de253f0e1278649e11492bde404ec930f
LnrdRmj <leonardo.ra14@icloud.com>
2022-03-17 12:58:36 +0100

    Added Icons

---
+++ src/components/AnimeAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  
+    <div class="rounded-md">
+        
+        <div class="text-xl">
+
+            <Icon icon="akar-icons:plus"/>
+
+        </div>
+
+    
+    </div>
+
+</template>
+
+<script>
+
+import { Icon } from '@iconify/vue';
+
+export default {
+
+    components: {
+        Icon
+    }
+
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
