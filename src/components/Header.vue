a32c3e1a73056338496aaf1c48d78a92a04b9c48
LnrdRmj <leonardo.ra14@icloud.com>
2022-03-27 11:29:56 +0200

    Header redirect to home page now

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
 
-    <div class="font-bold 
-        text-5xl md:text-6xl lg:text-7xl
-        text-center p-10
-        duration-200 text-secondary-900
-        ">
-        Your anime list
-    </div>
+    <router-link to="/">
+
+        <div class="font-bold 
+            text-5xl md:text-6xl lg:text-7xl
+            text-center p-10
+            duration-200 text-secondary-900
+            ">
+            Your anime list
+        </div>
+    
+    </router-link>
+
 
 </template>
 
