c7aa08a96af4043e138c9e928560479f03981ff0
LnrdRmj <leonardo.ra14@icloud.com>
2022-04-02 10:57:58 +0200

    Rounded action buttons

--- src/components/ActionButtons/ActionButton.vue
+++ src/components/ActionButtons/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   
-    <div class="bg-tertiary rounded-sm p-1">
+    <div class="bg-tertiary p-1" :class="classes">
         <Icon :icon="icon || 'emojione:question-mark'" class="text-3xl"/>
     </div>
 
@@ -11,9 +11,29 @@
 import { Icon } from "@iconify/vue"
 
 export default {
-    props: ['icon'],
+    props: ['icon', 'round'],
+    data: function () {
+        return {
+            borderStyle: ''
+        }
+    },
     components: {
         Icon
+    },
+    created: function () {
+
+        if (this.$props.round === true)
+            this.borderStyle = 'rounded-full'
+        else
+            this.borderStyle = 'rounded-sm'
+
+    },
+    computed:{
+        classes: function () {
+
+            return this.borderStyle;
+
+        }
     }
 }
 </script>
