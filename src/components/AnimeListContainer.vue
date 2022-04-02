b04f5515b26755944b980fae58146372a3f43edc
LnrdRmj <leonardo.ra14@icloud.com>
2022-04-02 09:24:01 +0200

    Update AnimeListContainer.vue

--- src/components/AnimeListContainer.vue
+++ src/components/AnimeListContainer.vue
@@ -1,14 +1,23 @@
 <template>
     
-    <div class="p-10">
+    <div class="p-10 flex flex-col items-center">
 
         <!-- Per fare il bordo gradient -->
         <!-- <div class="bg-white p-[10px] my-10 -z-10
                     rounded-md
                     bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 -->
             <!-- Container delle card -->
+
+            <div class="flex justify-evenly items-center 
+                        w-full h-5">
+                
+                <div class="border-2 border-solid mb-8 border-secondary-900 w-full mx-10"></div>
+
+            </div>
+
+
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-16 gap-y-10
-                        border-4 border-solid border-secondary-900 rounded-xl
+                        border-4 border-solid border-secondary-900 border-b-0 border-t-0
                         p-5 duration-75">
 
 
