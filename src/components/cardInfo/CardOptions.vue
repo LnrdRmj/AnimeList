e48ccc061d364691862809e5160bd8f7f96ccf44
LnrdRmj <leonardo.ra14@icloud.com>
2022-04-03 20:38:19 +0200

    Made a first options coll thing

--- src/components/cardInfo/CardOptions.vue
+++ src/components/cardInfo/CardOptions.vue
@@ -1,10 +1,32 @@
 <template>
   
-    <div @click.prevent="showOptions($event)"
+    <div 
         class=" absolute right-0 top-0 translate-x-1/2 -translate-y-1/2
-                duration-500 focus:rotate-45 active:rotate-[180deg]">
+                duration-500">
 
-        <ActionButton  icon='ci:settings-filled' :round="true"/>
+        <div class="relative">
+ 
+            <div hidden class="peer-focus:top-[40px] peer-focus:top-[80px]"></div>
+
+            <ActionButton   icon='ci:settings-filled' :round="true" tabindex="1"
+                            @click.prevent="showOptions($event)"
+                            class="z-10 focus:rotate-180 duration-500 peer"
+                            />
+
+            <ActionButton v-for="(option, index) in options" :key="option.icon"
+                            :icon="option.icon"
+                            ref="options"
+                            :round="true"
+                            @click="option.onclick"
+                            class="absolute top-0 -z-10
+                                    peer-focus:top-20
+                                    peer-focus:rotate-[360deg]
+                                    duration-500
+                            "
+                            :class='"peer-focus:top-[" + ( (index + 1) * 40) + "px]"'
+                            />
+
+        </div>
 
     </div>
 
@@ -20,11 +42,22 @@ export default {
     },
     data: function () {
         return {
-
+            options: [
+                {
+                    name: 'like',
+                    icon: 'akar-icons:heart',
+                    onclick: this.like
+                },
+                {
+                    name: 'delete',
+                    icon: 'fluent:delete-28-regular',
+                    onclick: this.delete
+                }
+            ]
         }
     },
     methods: {
-        showOptions : function (event) {
+        showOptions : function () {
 
             this.applyCSSChanges()
 
@@ -33,7 +66,17 @@ export default {
 
 
 
-        }
+        },
+        like: function (){
+
+            console.log('like on anime');
+
+        },
+        delete: function (){
+
+            console.log('delete on anime');
+
+        } 
     }
 }
 </script>
